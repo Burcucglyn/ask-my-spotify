bbde8ae66a251cf95654521018cb34fc41831b35
Burdzhu Chaglayan <burcucglyn95@gmail.com>
2026-06-09 00:02:26 +0100

    created the paths, and defined the get_client

---
+++ scripts/fetch.py
@@ -0,0 +1,40 @@
+##this is the file for fetching my recent played songs.
+
+#start with python libraries, as usuall drill.
+import json
+import os
+from datetime import datetime, timezone
+from pathlib import Path
+
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# reads .env into os.environ. without this the credentials are nowhere to be found.
+load_dotenv()
+
+#adding the paths without hard coding.. parent..
+REPO_ROOT = Path(__file__).resolve().parent.parent
+
+#raw json path once i fetch thats where it will be save
+RAW_DIR = REPO_ROOT/"data"/"raw"/"recently_played"
+CACHE_PATH = REPO_ROOT/".cache"/"spotify_token.json"
+
+#permission spotify recent-played in the first batch is 50, top-read= my number ones.. lib-read the songs I saved..
+SCOPES = "user-read-recently-played user-top-read user-library-read"
+
+#define get client to spotip handles whole oauth
+
+def get_client()-> spotipy.Spotify:
+    #1. run will open a tab-browser after token cache makes it silent.. very smart of me..
+    CACHE_PATH.parent.mkdir(exist_ok=True)
+    auth = SpotifyOAuth(
+        client_id=os.environ["SPOTIFY_CLIENT_ID"]
+        Client_secret=os.environ["SPOTIFY_CLIENT_SECRET"],
+        redirect_uri=os.environ["SPOTIFY_REDIRECT_URI"],
+        scope=SCOPES,
+        cache_path=str(CACHE_PATH),
+    )
+    return spotipy.Spotify(auth_manager=auth)
+    
+
